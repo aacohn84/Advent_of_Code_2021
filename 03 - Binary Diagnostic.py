706c8d91f3bbafcd747333726c5ee0d0191e907f
Aaron Cohn <aaron.cohn@workday.com>
2021-12-05 16:25:39 -0800

    Day 3 part 2

--- 03 - Binary Diagnostic.py
+++ 03 - Binary Diagnostic.py
@@ -15,6 +15,22 @@ def get_most_common_digit(diag, i):
         else: zeroes += 1
     return ones > zeroes
 
+def get_rows_for_digit_criteria(diag, i, most_common):
+    # diag - a subset of the diagnostic report
+    # i - the list position to inspect - must be between 0 and len(diag) - 1 inclusive
+    # most_common - boolean
+    # returns the rows with the most common digit in the 'i' position if most_common is set, or 1's if there is no most common digit
+    # else, returns rows with the least common digit in the 'i' position, or 0's if there is no least common digit
+    ones = []
+    zeroes = []
+    for row in diag:
+        if row[i] == '1': ones.append(row)
+        else: zeroes.append(row)
+    if most_common:
+        return ones if len(ones) >= len(zeroes) else zeroes
+    else:
+        return zeroes if len(zeroes) <= len(ones) else ones
+
 def binary_to_decimal(b, inverse = 0):
     l = len(b)
     acc = 0
@@ -23,8 +39,7 @@ def binary_to_decimal(b, inverse = 0):
         acc += ((b[i] + inverse) % 2) * 2**p
     return acc
 
-def main():
-    diag = process_diagnostic(read_input(('03 - Diagnostic Report.txt')))
+def part1(diag):
     num_digits = len(diag[0])
     gamma_rate = []
     for i in range(num_digits):
@@ -33,11 +48,33 @@ def main():
     epsilon_rate_dec = binary_to_decimal(gamma_rate, inverse=1)
     print(str(gamma_rate_dec * epsilon_rate_dec))
 
+def get_rows_for_digit_criteria_recursive(diag, i, most_common):
+    if len(diag) == 1 or i >= len(diag[0]):
+        return list(map(lambda s: int(s), diag[0]))
+    else:
+        rows = get_rows_for_digit_criteria(diag, i, most_common)
+        assert(i + 1 < len(diag[0]) or len(rows) == 1) # don't go past the end of the row
+        return get_rows_for_digit_criteria_recursive(rows, i + 1, most_common)
+
+def part2(diag):
+    num_digits = len(diag[0])
+    most_common = get_rows_for_digit_criteria_recursive(diag, 0, most_common=True)
+    least_common = get_rows_for_digit_criteria_recursive(diag, 0, most_common=False)
+    oxygen_rating = binary_to_decimal(most_common)
+    c02_rating = binary_to_decimal(least_common)
+    print(str(oxygen_rating * c02_rating))
+
+def main():
+    diag = process_diagnostic(read_input(('03 - Diagnostic Report.txt')))
+    part1(diag)
+    part2(diag)
+
 def run_tests():
     test_binary_to_decimal()
-    test_algorithm()
+    test_algorithm_1()
+    test_algorithm_2()
 
-def test_algorithm():
+def test_algorithm_1():
     input = read_input('03 - Unit Test Input.txt')
     diag = process_diagnostic(input)
     num_digits = len(diag[0])
@@ -51,6 +88,19 @@ def test_algorithm():
     gamma_rate_dec = binary_to_decimal(gamma_rate)
     epsilon_rate_dec = binary_to_decimal(gamma_rate, inverse=1)
     assert(gamma_rate_dec * epsilon_rate_dec == 198)
+
+def test_algorithm_2():
+    input = read_input('03 - Unit Test Input.txt')
+    diag = process_diagnostic(input)
+    most_common = get_rows_for_digit_criteria_recursive(diag, 0, most_common=True)
+    assert(most_common == [1,0,1,1,1])
+    least_common = get_rows_for_digit_criteria_recursive(diag, 0, most_common=False)
+    assert(least_common == [0,1,0,1,0])
+    oxygen_rating = binary_to_decimal(most_common)
+    assert(oxygen_rating == 23)
+    c02_rating = binary_to_decimal(least_common)
+    assert(c02_rating == 10)
+    assert(oxygen_rating * c02_rating == 230)
 
 def test_binary_to_decimal():
     b = [1,1,1]
